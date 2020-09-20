5c7b8eea2742d8dbe1d7637a19ede36a5941ed67
jefflomacy <57078432+jefflomacy@users.noreply.github.com>
2020-09-20 16:44:08 -0400

    nav and route

--- routes/towns.js
+++ routes/towns.js
@@ -5,24 +5,46 @@
 const express = require('express');
 
 /**
+ * Sanitizer.
+ */
+const sanitize = require('../helpers/sanitize');
+
+/**
  *
  * @type {Router}
  */
 const router = express.Router();
 
+/**
+ * Main page renderer.
+ * @param req
+ * @param res
+ * @param next
+ */
 function callBrowser(req, res, next) {
+    const pageNumber = req.params ? req.params.pageNumber : undefined;
+    const pageNumberInt = sanitize.parsePositiveInteger(pageNumber);
+
     const data = {};
     data.pageTitle = 'Dream Towns';
     data.pageDescription = 'Search for the best dreams to visit at VillagerDB';
+    data.pageNumber = pageNumberInt;
+    data.searchQuery = req.query.q ? req.query.q : '';
     res.render('towns/browser', data);
 }
 
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
     callBrowser(req, res, next);
 });
 
-router.get('/page/:pageNumber', function (req, res, next) {
+router.get('/page/:pageNumber',  (req, res, next) => {
     callBrowser(req, res, next);
 });
 
+/**
+ * Actual worker endpoint called by React component.
+ */
+router.get('/ajax', (req, res, next) => {
+    
+});
 module.exports = router;
